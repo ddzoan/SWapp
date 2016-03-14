810ce22d385490cb4298d80e36909d3655077da0
Dan Dzoan <dandzoan@gmail.com>
2016-03-14 00:32:19 -0700

    change method of exec as ddzoan

--- installer.sh
+++ installer.sh
@@ -3,30 +3,29 @@
 # first need to:
 # curl https://raw.githubusercontent.com/ddzoan/SWapp/master/installer.sh | PASSWORD=password bash
 
-if [ $UID -eq 0 ]; then
-  apt-get install git -y
-  apt-add-repository ppa:brightbox/ruby-ng -y
-  apt-get update -y
-  apt-get install ruby2.2 -y
+apt-get install git -y
+apt-add-repository ppa:brightbox/ruby-ng -y
+apt-get update -y
+apt-get install ruby2.2 -y
 
-  apt-get install ruby-dev zlib1g-dev liblzma-dev -y # for nokogiri
-  apt-get install libsqlite3-dev -y # needed for sqlite3
+apt-get install ruby2.2-dev zlib1g-dev liblzma-dev -y # for nokogiri
+apt-get install libsqlite3-dev -y # needed for sqlite3
 
-  gem install bundler
+gem install bundler
 
-  useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
-  # useradd --create-home -s /bin/bash ddzoan
-  usermod -a -G sudo ddzoan
+echo "adding user ddzoan"
+useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
+usermod -a -G sudo ddzoan
 
-  exec su ddzoan "$0"
-fi
-
+echo "run stuff as ddzoan"
+sudo -u ddzoan bash <<STR
 cd /home/ddzoan
 git clone https://github.com/ddzoan/SWapp.git swapp
 cd swapp
 
 # install gems
-bundle install
+bundle install --path vendor/bundle
 
 # create DB
 rake migrate
+STR
