cdce8965698576b8807451b741a504e2a63a98d1
Dan Dzoan <dandzoan@gmail.com>
2016-03-14 00:10:01 -0700

    run root install stuff and then exec rest as ddzoan user

--- installer.sh
+++ installer.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # first need to:
-# curl https://raw.githubusercontent.com/ddzoan/SWapp/master/installer.sh | bash
+# curl https://raw.githubusercontent.com/ddzoan/SWapp/master/installer.sh | PASSWORD=password bash
 
-apt-get install git -y
-apt-add-repository ppa:brightbox/ruby-ng -y
-apt-get update -y
-apt-get install ruby2.2 -y
+if [ $UID -eq 0 ]; then
+  apt-get install git -y
+  apt-add-repository ppa:brightbox/ruby-ng -y
+  apt-get update -y
+  apt-get install ruby2.2 -y
 
-apt-get install ruby-dev zlib1g-dev liblzma-dev -y # for nokogiri
-apt-get install libsqlite3-dev -y # needed for sqlite3
+  apt-get install ruby-dev zlib1g-dev liblzma-dev -y # for nokogiri
+  apt-get install libsqlite3-dev -y # needed for sqlite3
 
-gem install bundler
+  gem install bundler
 
-# useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
-useradd --create-home -s /bin/bash ddzoan
-usermod -a -G sudo ddzoan
+  useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
+  # useradd --create-home -s /bin/bash ddzoan
+  usermod -a -G sudo ddzoan
 
-su - ddzoan
+  exec su ddzoan "$0"
+fi
+
 cd /home/ddzoan
 git clone https://github.com/ddzoan/SWapp.git swapp
 cd swapp
