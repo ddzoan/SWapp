6155b0d92269b9c2228d3ab3407a2efa68e9f25f
Dan Dzoan <dandzoan@gmail.com>
2016-03-13 22:51:05 -0700

    automatically say yes in script

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # PASSWORD=password
 # curl https://raw.githubusercontent.com/ddzoan/SWapp/master/installer.sh | bash
 
-apt-get install git
+apt-get install git -y
 apt-add-repository ppa:brightbox/ruby-ng -y
 apt-get update -y
 apt-get install ruby2.2 -y
@@ -12,10 +12,11 @@ apt-get install libsqlite3-dev -y # needed for sqlite3
 
 gem install bundler
 
-useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
+# useradd --create-home -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) ddzoan
+useradd --create-home -s /bin/bash ddzoan
 usermod -a -G sudo ddzoan
 
-su ddzoan
+su - ddzoan
 cd /home/ddzoan
 git clone https://github.com/ddzoan/SWapp.git swapp
 cd swapp
